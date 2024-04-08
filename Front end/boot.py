01822ed1c2b4c1188f36257d702d8fe058ad28b0
Alessandro-45 <posixgen@gmail.com>
2024-04-07 21:22:28 -0400

    Chicos, en estos programas que he creado hice un boot.py que lo que hace es iniciar toda la configuracion del ESP32.

---
+++ Front end/boot.py
@@ -0,0 +1,31 @@
+from machine import Pin as Pn
+import esp
+import gc
+import network 
+
+try: 
+    import usocket as uskt
+except:
+    import socket
+
+esp.osdebug(None)
+gc.collect()
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('lab laser 2', '160,Lab.Laser!')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
+
+pin33 = Pin(33, Pin.OUT)
+pin25 = Pin(25, Pin.OUT)
+pin34 = Pin(34, Pin.OUT)
+pin35 = Pin(35, Pin.OUT)
+pin32 = Pin(32, Pin.OUT)
+
